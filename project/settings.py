8f9512213f5f024c8abd0c3903e02f3922d6cd9c
gaurav <gauravsharma2696@gmail.com>
2018-02-13 02:34:13 +0530

    Fixed some stuff

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bvbrsz-2ga^exnv2vzb%($64na0xavapy)xe%)*sm5+v!r*xf6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.182.139','0.0.0.0']
+ALLOWED_HOSTS = ['192.168.182.140','0.0.0.0']
 
 
 # Application definition
--- staticfiles/main.js
+++ staticfiles/main.js
@@ -1,6 +1,5 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-var recognition = new SpeechRecognition();
-recognition.lang = 'en-IN';
+var SpeechRecognition = '';
+var recognition = '';
 
 $(document).ready(function(){
 	$("#sendTranslate").click(function(){
@@ -23,6 +22,7 @@ $(document).ready(function(){
 		});
 	});
 	$("#startVoice").click(function(){
+		startRecording();
 		$("#result").html('');
 		recognition.start();
   		console.log('Voice recognition Started');
@@ -30,60 +30,66 @@ $(document).ready(function(){
 	
 });
 
-recognition.onresult = function(event) {
-	// The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-	// The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-	// It has a getter so it can be accessed like an array
-	// The [last] returns the SpeechRecognitionResult at the last position.
-	// Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-	// These also have getters so they can be accessed like arrays.
-	// The [0] returns the SpeechRecognitionAlternative at position 0.
-	// We then return the transcript property of the SpeechRecognitionAlternative object
+function startRecording(){
+	SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+	recognition = new SpeechRecognition();
+	recognition.lang = 'en-IN';
 
-	var last = event.results.length - 1;
-	var result = event.results[last][0].transcript;
+	recognition.onresult = function(event) {
+		// The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+		// The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+		// It has a getter so it can be accessed like an array
+		// The [last] returns the SpeechRecognitionResult at the last position.
+		// Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+		// These also have getters so they can be accessed like arrays.
+		// The [0] returns the SpeechRecognitionAlternative at position 0.
+		// We then return the transcript property of the SpeechRecognitionAlternative object
 
-	console.log('Result: ' + result);
-	console.log('Confidence: ' + event.results[0][0].confidence);
-	
-	console.log(event.results)
+		var last = event.results.length - 1;
+		var result = event.results[last][0].transcript;
 
-	console.log("HOST IS - "+host);
-	var url="http://"+host+":8081/translate";
-	
-	var lang='hi';
-	$("#voiceResults").html('<div class="loader"></div>');
-	$("#translatedVoiceResults").html('');
-	
-	$.ajax({
+		console.log('Result: ' + result);
+		console.log('Confidence: ' + event.results[0][0].confidence);
 		
-		type: "POST",
-		url: url,
-		data: {text:result,lang:lang},
-		success: function(result){
-			
-			console.log("Voice Result :"+result);
-			$("#voiceResults").html(result);
-			
-			var lang='en';
-			$("#translatedVoiceResults").html('');
-			
-			$.ajax({
-				type: "POST",
-				url: url,
-				data: {text:result,lang:lang},
-				success: function(result){
-					console.log("Translated Voice Result :"+result);
-					$("#translatedVoiceResults").html(result);
-				}
-			});
+		console.log(event.results)
+
+		console.log("HOST IS - "+host);
+		var url="http://"+host+":8081/translate";
 		
-		}
-	
-	});
+		var lang='hi';
+		$("#voiceResults").html('<div class="loader"></div>');
+		$("#translatedVoiceResults").html('');
+		
+		$.ajax({
+			
+			type: "POST",
+			url: url,
+			data: {text:result,lang:lang},
+			success: function(result){
+				
+				console.log("Voice Result :"+result);
+				$("#voiceResults").html(result);
+				
+				var lang='en';
+				$("#translatedVoiceResults").html('');
+				
+				$.ajax({
+					type: "POST",
+					url: url,
+					data: {text:result,lang:lang},
+					success: function(result){
+						console.log("Translated Voice Result :"+result);
+						$("#translatedVoiceResults").html(result);
+					}
+				});
+			
+			}
+		
+		});
 
-}
+	}
 
-recognition.onspeechend = function() {
-  recognition.stop();
+	recognition.onspeechend = function() {
+	  recognition.stop();
+	}
 }
